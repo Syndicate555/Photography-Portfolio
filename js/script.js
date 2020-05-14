f45ceefd41663af5057c72802e1f76e187648719
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 03:57:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,3 +40,40 @@ $(function () {
     }
   });
 });
+
+window.sr = ScrollReveal();
+sr.reveal(".navbar", {
+  duration: 2000,
+  origin: "bottom",
+});
+sr.reveal(".showcase-left", {
+  duration: 2000,
+  origin: "top",
+  distance: "300px",
+});
+sr.reveal(".showcase-right", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+});
+sr.reveal(".showcase-btn", {
+  duration: 2000,
+  delay: 2000,
+  origin: "bottom",
+});
+sr.reveal("#testimonial div", {
+  duration: 2000,
+  origin: "bottom",
+});
+sr.reveal(".info-left", {
+  duration: 2000,
+  origin: "left",
+  distance: "300px",
+  viewFactor: 0.2,
+});
+sr.reveal(".info-right", {
+  duration: 2000,
+  origin: "right",
+  distance: "300px",
+  viewFactor: 0.2,
+});
