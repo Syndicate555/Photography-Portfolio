5374b71f6805e45dfe85e7f2cc7a16efa65b8bb5
Syndicate555 <saziz028@uottawa.ca>
2020-05-14 17:20:24 -0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+// 1
 var img1 = document.getElementById("myImg");
 var modalImg1 = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img1.onclick = function () {
+  modal.style.display = "block";
+  modalImg1.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// 2
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img02");
 var captionText = document.getElementById("caption");
 img1.onclick = function () {
   modal.style.display = "block";
